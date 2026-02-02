0a6b770dc60db891ea91f24651cf71ebb41dcd18
conradolandia <andresconrado@gmail.com>
2026-02-02 11:47:25 -0500

    Try to avoid flash on loading changelog, and add typography plugin to style general html prose with Tailwind

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         if (process.env.NODE_ENV !== 'production') {
           return html
             .replace(/__SHOW_CHANGELOG__/g, 'false')
-            .replace(/__CHANGELOG_HTML__/g, '"This will be replaced with a Python template variable in the dist folder."');
+            .replace(/__CHANGELOG_HTML__/g, '"This will be replaced with a Python template variable in the dist folder. <ul><li>This is a test</li><li>This is a test</li><li>This is a test</li></ul>"');
         }
         return html;
       }
