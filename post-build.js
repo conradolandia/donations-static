13fde45aa8e3ef131addef5a9e2e2fc51218784c
conradolandia <andresconrado@gmail.com>
2026-08-26 02:06:16 -0500

    Improve variable handling and design

--- post-build.js
+++ post-build.js
@@ -7,6 +7,8 @@ try {
   let html = readFileSync(distHtmlPath, 'utf8');
   
   // Replace placeholders with Python template syntax
+  html = html.replace(/__THEME_MODE__/g, '${theme_mode}');
+  html = html.replace(/__CSS_VARIABLES__/g, '${css_appeal}');
   html = html.replace(/__SHOW_CHANGELOG__/g, '${show_changelog}');
   html = html.replace(/__CHANGELOG_HTML__/g, '"${changelog_html}"');
   
